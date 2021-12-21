95ffa46ee35eb874259570d1c6b571925a751024
r4mand <amarinaa0@gmail.com>
2021-12-22 02:21:02 +0300

    fix

--- js/all.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const swiper = new Swiper('.swiper', {
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        allowTouchMove: false
-    });
-})
